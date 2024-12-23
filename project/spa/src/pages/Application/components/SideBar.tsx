e955084cf7c00b803c35fd9d7cfc7d47a11ae4f8
Dario Minoza <36031556+dminoza@users.noreply.github.com>
2024-12-23 09:38:38 +0800

    updated post for only some person

--- project/spa/src/pages/Application/components/SideBar.tsx
+++ project/spa/src/pages/Application/components/SideBar.tsx
@@ -2,11 +2,11 @@ import { RiHomeLine, RiInboxLine } from "react-icons/ri";
 import { PiSignOutLight, PiBooksThin } from "react-icons/pi";
 import { FaNotesMedical } from "react-icons/fa6";
 import { Modal, Button, Label, Textarea, FileInput } from "flowbite-react";
-import { HiOutlineExclamationCircle } from "react-icons/hi";
 import { useState } from "react";
 import { useAppDispatch } from "../../../hooks";
 import { mutateLoggedIn } from "../../authentication/Login/slice";
 import { useNavigate } from "react-router-dom";
+import { NavLink } from "react-router-dom";
 
 export default function SideBar () {
     const [openModal, setOpenModal] = useState(false)
@@ -22,25 +22,28 @@ export default function SideBar () {
                 </div>
                 {/* <img src={ process.env.PUBLIC_URL + 'USTP Logo against Dark Background.png' } className="App-logo" alt="logo" /> */}
                 <ul className="space-y-2 font-medium">
-                    <li onClick={() => navigate('/')}>
-                        <a href="#" className="flex items-center p-2 text-white rounded-lg dark:text-white dark:hover:bg-gray-700 group">
-                        <RiHomeLine className="flex-shrink-0 w-5 h-5 text-gray-500 transition duration-75 dark:text-gray-400 group-hover:text-white dark:group-hover:text-white" />
-                        <span className="ms-3">Home</span>
-                        </a>
+                    <li>
+                        <NavLink to="/"  className={({isActive}) => (isActive) ? "flex items-center p-2 rounded-lg text-white dark:text-white dark:hover:bg-gray-700 group" : "flex items-center p-2 rounded-lg text-gray-500 hover:text-white dark:text-white dark:hover:bg-gray-700 group"}>
+                            <RiHomeLine className="flex-shrink-0 w-5 h-5 transition duration-75 dark:text-gray-400 group-hover:text-white dark:group-hover:text-white" />
+                            <span className="ms-3">Home</span>
+                        </NavLink>
+                        {/* <a href="#" className="flex items-center p-2 text-white rounded-lg dark:text-white dark:hover:bg-gray-700 group">
+                        
+                        </a> */}
                     </li>
                     <li>
-                        <a href="#" className="flex items-center p-2 text-white rounded-lg dark:text-white dark:hover:bg-gray-700 group">
+                        <a href="#" className="flex items-center p-2 text-gray-500 rounded-lg dark:text-white dark:hover:bg-gray-700 group">
                         <RiInboxLine className="flex-shrink-0 w-5 h-5 text-gray-500 transition duration-75 dark:text-gray-400 group-hover:text-white dark:group-hover:text-white" />
                         <span className="flex-1 ms-3 whitespace-nowrap">Inbox</span>
                         <span className="inline-flex items-center justify-center w-3 h-3 p-3 ms-3 text-sm font-medium text-blue-800 bg-blue-100 rounded-full dark:bg-blue-900 dark:text-blue-300">3</span>
                         </a>
                     </li>
                     <li onClick={() => navigate('lms')}>
-                        <a href="#" className="flex items-center p-2 text-white rounded-lg dark:text-white dark:hover:bg-gray-700 group">
-                        <PiBooksThin className="flex-shrink-0 w-5 h-5 text-gray-500 transition duration-75 dark:text-gray-400 group-hover:text-white dark:group-hover:text-white" />
-                        <span className="flex-1 ms-3 whitespace-nowrap">LMS</span>
-                        {/* <span className="inline-flex items-center justify-center w-3 h-3 p-3 ms-3 text-sm font-medium text-blue-800 bg-blue-100 rounded-full dark:bg-blue-900 dark:text-blue-300">3</span> */}
-                        </a>
+                        <NavLink to="lms" className={({isActive}) => (isActive) ? "flex items-center p-2 rounded-lg text-white dark:text-white dark:hover:bg-gray-700 group" : "flex items-center p-2 rounded-lg text-gray-500 hover:text-white dark:text-white dark:hover:bg-gray-700 group"}>
+                            <PiBooksThin className="flex-shrink-0 w-5 h-5 transition duration-75 dark:text-gray-400 group-hover:text-white dark:group-hover:text-white" />
+                            <span className="flex-1 ms-3 whitespace-nowrap">LMS</span>
+                            {/* <span className="inline-flex items-center justify-center w-3 h-3 p-3 ms-3 text-sm font-medium text-blue-800 bg-blue-100 rounded-full dark:bg-blue-900 dark:text-blue-300">3</span> */}
+                        </NavLink>
                     </li>
                     <li onClick={() => setOpenModal(true)}>
                         <a href="#" className="flex items-center p-2 text-white rounded-lg dark:text-white dark:hover:bg-gray-700 group">
--- project/spa/src/pages/Application/pages/posts/components/post.tsx
+++ project/spa/src/pages/Application/pages/posts/components/post.tsx
@@ -1,6 +1,10 @@
 import { Avatar } from "flowbite-react";
+import { useState } from "react";
 
 export default function PostMain () {
+
+    const [readMore, setReadMore] = useState(false)
+
     return (
         <div className="m-4">
             <div className="">
@@ -11,8 +15,9 @@ export default function PostMain () {
                     <Avatar rounded />
                 </div>
                 <div className="text-white max-w-96">
-                    <p className="truncate overflow-hidden">Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.</p>
-                    <a href="#" className="text-yellow">Comment</a>
+                    <p className={(readMore) ? "" : "truncate overflow-hidden"}>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.</p>
+                    <span className="text-blue-500" onClick={() => setReadMore(!readMore)}>read { (readMore) ? `less` : `more` }</span>
+                    {/* <a href="#" className="text-yellow">Comment</a> */}
                 </div>
             </div>
         </div>

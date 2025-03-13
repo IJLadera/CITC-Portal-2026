a0349dd34a7ac18b7c4ce0b29ddf504d8ad1e79b
Jayce Tabobo <tabobo.jayce01@gmail.com>
2025-03-13 13:44:58 +0800

    adding update profile and change password

--- project/app/users/urls.py
+++ project/app/users/urls.py
@@ -1,6 +1,10 @@
 from django.urls import path, include
+from .views import UpdateProfileView, ChangePasswordView
 
 urlpatterns = [
     path('', include('djoser.urls')),
     path('', include('djoser.urls.authtoken')),
+
+    path('update/', UpdateProfileView.as_view(), name='update-profile'),
+    path('change-password/', ChangePasswordView.as_view(), name='change-password'),
 ]
--- project/app/users/views.py
+++ project/app/users/views.py
@@ -1,3 +1,16 @@
 from django.shortcuts import render
+from rest_framework.generics import CreateAPIView, UpdateAPIView
+from rest_framework import permissions
+from .models import User
+from .serializers import CreateUserSerializer, UpdateUserSerializer, ChangePasswordSerializer
+from .permissions import IsUserOrIsAdminOrReadOnly
 
-# Create your views here.
+class UpdateProfileView(UpdateAPIView):
+    queryset = User.objects.all()
+    permission_classes = [permissions.IsAuthenticated, IsUserOrIsAdminOrReadOnly]
+    serializer_class = UpdateUserSerializer
+
+class ChangePasswordView(UpdateAPIView):
+    queryset = User.objects.all()
+    permission_classes = [permissions.IsAuthenticated, IsUserOrIsAdminOrReadOnly]
+    serializer_class = ChangePasswordSerializer
